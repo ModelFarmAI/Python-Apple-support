79b84099823e3c2321726ef298c95054fe8a911c
Ali Beyram <alibeyram@users.noreply.github.com>
2023-09-05 15:45:56 -0700

    Update metapath-sitecustomize.py
    editing framework name for name collision possibility

--- patch/Python/metapath-sitecustomize.py
+++ patch/Python/metapath-sitecustomize.py
@@ -14,7 +14,8 @@ filename_extension = [x for x in EXTENSION_SUFFIXES if bool(re.search(r"\.cpytho
 class MyMetaPathFinder(MetaPathFinder):
     def find_spec(self, fullname, path, target=None):
         name = fullname.split(".")[-1]
-        file_abs_path = os.path.join(Frameworks_loc, name + ".framework", name + filename_extension)
+        framework_name = "_".join(fullname.split("."))
+        file_abs_path = os.path.join(Frameworks_loc, f"{framework_name}.framework",f"{name}{filename_extension}")
 
         if os.path.exists(file_abs_path):
             loader = ExtensionFileLoader(fullname, file_abs_path)
